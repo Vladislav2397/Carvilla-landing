8ffc449823a62acf32cb3c85719a8cfd5677a4c0
Vladislav2397 <valdiszz53@gmail.com>
2023-10-31 13:27:02 +0300

    Add assets and install express

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // run `node index.js` in the terminal
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
