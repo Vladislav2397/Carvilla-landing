5bb2bacbca1974837d2824433c2b8568382f256d
Vladislav2397 <valdiszz53@gmail.com>
2023-10-31 21:05:57 +0300

    Update assets

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 })
 
+app.use(express.static(__dirname + '/public'))
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
